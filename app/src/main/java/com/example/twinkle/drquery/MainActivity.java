13842e30e2ff200f3ef32bf585c7d0592722da60
twinkle13 <twinkle.kheterpal@gmail.com>
2017-04-25 17:24:44 +0530

    Splash screen Complete

---
+++ app/src/main/java/com/example/twinkle/drquery/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.twinkle.drquery;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    private ProgressBar spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        spinner=(ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}

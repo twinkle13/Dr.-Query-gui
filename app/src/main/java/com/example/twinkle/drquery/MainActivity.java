ee50698f63d14fb3d3ae20672235ad07e35aee83
twinkle13 <twinkle.kheterpal@gmail.com>
2017-04-29 14:31:54 +0530

    Splash screen Complete and main activity started2

--- app/src/main/java/com/example/twinkle/drquery/MainActivity.java
+++ app/src/main/java/com/example/twinkle/drquery/MainActivity.java
@@ -3,26 +3,30 @@ package com.example.twinkle.drquery;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+//    Button b1;
     private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=(Button)findViewById(R.id.button);
+//        b1=(Button)findViewById(R.id.button);
         spinner=(ProgressBar)findViewById(R.id.progressBar);
         spinner.setVisibility(View.GONE);
+        startSpinner();
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                spinner.setVisibility(View.VISIBLE);
-            }
-        });
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                spinner.setVisibility(View.VISIBLE);
+//            }
+//        });
+    }
+    private void startSpinner()
+    {
+        spinner.setVisibility(View.VISIBLE);
     }
 }
